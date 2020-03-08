e1e2ec95fef6535d465baf83a26498e75a0304a3
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-08 13:54:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def using_include(array, element)
   array.include?(element)
 end
 
-def using_sort(sports)
+def using_sort(array, element)
   sports = ["Knicks","Nets","Falcons","Yankess"]
   sports.sort
 end
