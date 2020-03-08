e2474bc967d670ba950e1795d9c87e2a30796877
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-08 13:53:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
-def using_sort(array)
-
+def using_sort(sports)
+  sports = ["Knicks","Nets","Falcons","Yankess"]
+  sports.sort
 end
 
 def using_reverse(array)
-
+  sports = ["Knicks","Nets","Falcons","Yankess"]
+  sports.reverse
 end
 
 def using_first(array)
-
+  sports = ["Knicks","Nets","Falcons","Yankess"]
+  sports.first
 end
 
 def using_last(array)
-
+  sports = ["Knicks","Nets","Falcons","Yankess"]
+  sports.last
 end
 
 def using_size(array)
-
-end
+sports = ["Knicks","Nets","Falcons","Yankess"]
+end 
