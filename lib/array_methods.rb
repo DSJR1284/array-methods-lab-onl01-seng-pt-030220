a583d63fae9659d3e23006e790f7aecd363cb0f2
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-08 14:06:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def using_include(array, element)
   array.include?(element)
 end
 
-def using_sort(array, element)
+def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.sort(array)
 end
